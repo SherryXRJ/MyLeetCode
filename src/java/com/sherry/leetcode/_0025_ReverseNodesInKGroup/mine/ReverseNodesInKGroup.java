39980cc0543bf176162223758f8fe07008731bca
xurunjie <xurunjie@deepexi.com>
2019-10-22 18:25:09 +0800

    解决25题

---
+++ src/java/com/sherry/leetcode/_0025_ReverseNodesInKGroup/mine/ReverseNodesInKGroup.java
@@ -0,0 +1,85 @@
+package com.sherry.leetcode._0025_ReverseNodesInKGroup.mine;
+
+/**
+ * 25. K 个一组翻转链表
+ * <p>
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ */
+public class ReverseNodesInKGroup {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode result = new ListNode(0);
+        result.next = head;
+        this.reverseListNode(result, head, k);
+
+        return result.next;
+    }
+
+    /**
+     * 反转局部链表
+     * @param pre   局部链表前一个节点
+     * @param list  需要翻转的链表
+     * @param size  翻转节点个数
+     */
+    private void reverseListNode(ListNode pre, ListNode list, int size) {
+        ListNode checkNode = pre;
+
+        //  原始头
+        ListNode originHead = list;
+
+        //  原始尾
+        ListNode originTail = null;
+
+
+        for (int i = 0; i < size; i++) {
+            //  链表节点个数不够 直接返回
+            checkNode = checkNode.next;
+            if (checkNode == null) return;
+            originTail = checkNode;
+        }
+
+        // 原始尾的下一个节点
+        ListNode after = originTail == null ? null : originTail.next;
+
+        ListNode current = list;
+        ListNode temp = list;
+        ListNode oriNext;
+        //  翻转
+        for (int i = 0; i < size; i++) {
+            oriNext = current.next;
+            current.next = temp;
+            temp = current;
+            current = oriNext;
+        }
+
+        pre.next = originTail;
+        originHead.next = after;
+
+        this.reverseListNode(originHead, originHead.next, size);
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+
+        ReverseNodesInKGroup demo = new ReverseNodesInKGroup();
+        ListNode result = demo.reverseKGroup(node, 3);
+        System.out.println(result);
+    }
+
+
+}
