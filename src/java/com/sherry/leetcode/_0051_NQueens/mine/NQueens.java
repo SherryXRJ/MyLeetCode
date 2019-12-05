73ff6239934fb3d44179c4d583803a9062e36534
xurunjie <xurunjie@deepexi.com>
2019-12-05 16:29:01 +0800

    参考他人题解，理解解决51

--- src/java/com/sherry/leetcode/_0051_NQueens/mine/NQueens.java
+++ src/java/com/sherry/leetcode/_0051_NQueens/mine/NQueens.java
@@ -1,8 +1,8 @@
 package com.sherry.leetcode._0051_NQueens.mine;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 51. N皇后
@@ -13,8 +13,7 @@ public class NQueens {
 
     private List<List<String>> result = new ArrayList<>();
 
-    private int[][] chessboard;
-
+    ////////////////////这3个字段是状态校验///////////////////////////
     /**
      * 列
      */
@@ -29,52 +28,75 @@ public class NQueens {
      * 负斜率
      */
     private int[] negativeSlope;
+    ///////////////////////////////////////////////////////////////
+
+    /**
+     * n个棋子，元素值表示第几列的棋子
+     */
+    private int[] nums;
+
+    /**
+     * 皇后
+     *
+     * 栈下标表示棋盘第几行，元素表示在该行第几列
+     */
+    private Stack<Integer> queens = new Stack<>();
 
     public List<List<String>> solveNQueens(int n) {
 
         col = new int[n];
         positiveSlope = new int[(n << 1) - 1];
         negativeSlope = new int[(n << 1) - 1];
-        int row[] = new int[n];
-        return null;
+
+        nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = i;
+        }
+
+        backtrack(n, 0);
+        return result;
     }
 
-    public void backtrack(int n, List<String> tempList, List<List<String>> curMap, int row, int queenCount){
+    public void backtrack(int n, int row){
+        if (n == row) {
+            List<String> rowStr = new ArrayList<>();
+            for (Integer e : queens) {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < n; i++) {
+                    stringBuilder.append(".");
+                }
+                stringBuilder.replace(e, e + 1, "Q");
+                rowStr.add(stringBuilder.toString());
+            }
+            result.add(rowStr);
+            return;
+        }
 
         for (int j = 0; j < n; j++) {
-            if (col[j] != 1 && positiveSlope[j + row] != 1 && negativeSlope[j - row] != 1) {
+            //  j - row + n - 1     防止下标越界(负数)
+            if (col[j] != 1 && positiveSlope[j + row] != 1 && negativeSlope[j - row + n - 1] != 1) {
+                //  放棋子
+                queens.add(nums[j]);
 
+                col[j] = 1;
+                positiveSlope[j + row] = 1;
+                negativeSlope[j - row + n - 1] = 1;
+
+                backtrack(n, row + 1);
+
+                //  回退回来
+                queens.pop();
+                col[j] = 0;
+                positiveSlope[j + row] = 0;
+                negativeSlope[j - row + n - 1] = 0;
             }
         }
-
-
-    }
-    private boolean checkRefreshChessboard(int x, int y) {
-        //  行、列检查
-        for (int i = 0; i < chessboard.length ; i++) {
-            if (x == i && y == i) continue;
-            if (chessboard[x][i] == 1) return false;
-            if (chessboard[i][y] == 1) return false;
-        }
-
-
-        //  正斜率
-        for (int i = 0; i < chessboard.length; i++) {
-            if (x == i && y == i) continue;
-            int j = x + y - i;
-            if (chessboard[i][j] == 1) return false;
-        }
-
-        //  负斜率
-        for (int i = 0, j = i + (y - x); i < chessboard.length; i++) {
-            if (x == i && y == i) continue;
-
-            if (chessboard[i][j] == 1) return false;
-        }
-
-        return true;
     }
 
-
+    public static void main(String[] args) {
+        NQueens demo = new NQueens();
+        List<List<String>> lists = demo.solveNQueens(4);
+        System.out.println(lists);
+    }
 
 }
