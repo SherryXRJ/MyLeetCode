c8c71be1f8bb7441da175a9ae8408bc0576ddc0d
xurunjie <xurunjie@cmii.chinamobile.com>
2021-03-18 09:41:57 +0800

    61解决

--- src/java/com/sherry/leetcode/_0061_RotateList/mine/RotateList.java
+++ src/java/com/sherry/leetcode/_0061_RotateList/mine/RotateList.java
@@ -3,8 +3,9 @@ package com.sherry.leetcode._0061_RotateList.mine;
 public class RotateList {
 
     public ListNode rotateRight(ListNode head, int k) {
+        //  特殊值处理
         if (head == null) return null;
-        if (head.next == null) return head;
+        if (head.next == null || k == 0) return head;
 
         ListNode oldHead = head;
         ListNode oldTail = null;
@@ -22,11 +23,20 @@ public class RotateList {
             temp = temp.next;
         }
 
+        //  k是size的整数倍 相当于链表没有移动 直接返回head
+        if (k % size == 0) {
+            return head;
+        }
+
+        //  把原来链表的接上
         oldTail.next = oldHead;
 
 
-        int index = Math.abs(size - k);
+        //  k != size的整数倍规律
+        int index = Math.abs(size - k % size);
 
+
+        //  找到新的head与新的tail， 将新的tail.next -> null
         temp = oldHead;
         for (int i = 0; i < size; i++) {
             if (index == i) {
@@ -64,7 +74,7 @@ public class RotateList {
         ListNode n1 = new ListNode(1, n2);
 
         RotateList demo = new RotateList();
-        ListNode node = demo.rotateRight(n1, 5);
+        ListNode node = demo.rotateRight(n3, 2);
         System.out.println(node);
     }
 }
